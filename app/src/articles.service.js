d133467877038219deecbdd9de53e0a9cff3a899
vitkuz <vitkuz.info@gmail.com>
2019-11-10 16:53:28 +0300

    update build spec

--- app/src/articles.service.js
+++ app/src/articles.service.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const services = require('./services');
 
 async function getArticles() {
-  const { ipV4: ip, port } = await services('users', '1.x.x');
+  const { ipV4, port } = await services('users', '1.x.x');
 
-  const response = await axios.get(`http://${ip}:${port}/articles`);
+  const response = await axios.get(`http://${ipV4}:${port}/articles`);
   return response.data;
 }
 
